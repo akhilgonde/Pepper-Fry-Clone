71cdaed14be3c0507464082d9ac897741bbda4e6
akhilgonde <akhil.gonde143@gmail.com>
2022-04-01 19:02:18 +0530

    header imported from componentes

---
+++ components/header.js
@@ -0,0 +1,30 @@
+const header = () =>
+{
+    return `<div id="disc">
+    <div>Shubh Aarambh Sale | Up to 60% Off | Use Code <span id="shubh">SHUBH</span></div> 
+    <div>Extra 20% Cashback On All Orders |T&amp;C Apply</span> <a href="https://www.pepperfry.com/promotions.html" ><span id="tc">SignUp</span></a></div>  
+   </div>
+   <div id="nav1">
+       <div id="nav11">
+           <div><a href="https://www.pepperfry.com/partner-with-us.html?partner=seller&amp;&amp;">Sell on Pepperfry</a></div>
+           <div><a href="https://www.pepperfry.com/franchise.html?partner=franchisee&amp;&amp;">Become a Franchisee</a></div>
+           <div><a href="https://www.pepperfry.com/bulkorder.html">Buy in Bulk</a></div>
+           <div><a href="https://www.pepperfry.com/studio-pepperfry.html">Find a Studio</a></div>
+       </div>
+       <div id="nav12">
+           <div id="pin">Enter Pincode</div>
+           <div><span id="tc">Find Pepperfry studio</span></div>
+       </div>
+   </div>
+   <div id="navMain">
+       <div><img src="https://ii1.pepperfry.com/images/svg/pf-logo-21.svg"></div>
+       <div id="input"><input id="search" placeholder="Your door to happiness opens with a search" type="text"></div>
+       <div id="navMain1">
+           <div><img src="https://ii1.pepperfry.com/images/svg/web21-header-help-icon.svg"></div>
+           <div><img src="https://ii1.pepperfry.com/images/svg/icon-profile-21.svg"></div>
+           <div><img src="https://ii1.pepperfry.com/images/svg/icon-wishlist-21.svg"></div>
+           <div><img src="	https://ii1.pepperfry.com/images/svg/icon-cart-21.svg"></div>
+       </div>`
+}
+
+export default header;
